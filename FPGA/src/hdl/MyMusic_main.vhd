f0424d48589922c331c3716afc570db13d6b7ba7
Keelin Wheeler <keelinwheeler@gmail.com>
2019-12-06 20:29:35 -0500

    Fixing small bug.

--- FPGA/src/hdl/MyMusic_main.vhd
+++ FPGA/src/hdl/MyMusic_main.vhd
@@ -142,7 +142,7 @@ begin
     inst_hex: hex_7seg
         port map(
             CLK => CLK,
-            DATA => note_value(0)&note_value(1)&note_value(2)&volume&"000"&playing,
+            DATA => note_value(0)&"00000000"&"00000000"&volume&"000"&playing,
             DIGIT_ENABLE => "11111111",
             SSEG_CA => SSEG_CA,
             SSEG_AN => SSEG_AN
